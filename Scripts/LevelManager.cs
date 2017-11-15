c8db8b4d0bd68a80d5186ef7bf6667f1280ac710
KoaleleS <33105619+KoaleleS@users.noreply.github.com>
2017-11-15 10:57:42 -1000

    Add files via upload

---
+++ Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log ("Level load requsted for: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest () {
+		Debug.Log("I Want to quit!");
+		Application.Quit();
+	}
+}
---
+++ Scripts/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+
+	int max;
+	int min;
+	int guess;
+
+	public Text text;
+	public int maxGuessesAllowed;
+	public Text guessesLeft;
+
+	void Start ()
+	{
+		StartGame ();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		maxGuessesAllowed = 16;
+		guessesLeft.text = maxGuessesAllowed.ToString();
+		NextGuess();
+
+	}
+
+	public void GuessLower (){
+		max = guess;
+		NextGuess ();
+	}
+
+	public void GuessHigher (){
+		min = guess;
+		NextGuess ();
+	}
+
+	void NextGuess (){
+		guess = Random.Range (min,max + 2);
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed -1;
+		guessesLeft.text = maxGuessesAllowed.ToString();
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
